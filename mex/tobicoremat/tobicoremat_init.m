43364333fae0018fdb6b1aaa7383114e989b13aa
Michele Tavella <tavella.michele@gmail.com>
2011-05-23 06:54:37 +0200

    Some changes

---
+++ mex/tobicoremat/tobicoremat_init.m
@@ -0,0 +1,5 @@
+[~, basepath] = mtpath_basename(which('tobicoremat_init.m'));
+
+mtpath_include([basepath '/mextobicore']);
+mtpath_include([basepath '/mextobiic']);
+mtpath_include([basepath '/mextobiid']);
