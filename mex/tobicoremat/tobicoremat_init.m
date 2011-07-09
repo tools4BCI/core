066e357e1794351ba1290ece760cc4b20a724cf7
Michele Tavella <tavella.michele@gmail.com>
2011-07-09 16:26:31 +0200

    Changed default mex path

--- mex/tobicoremat/tobicoremat_init.m
+++ mex/tobicoremat/tobicoremat_init.m
@@ -1,3 +1,14 @@
+% 2011-07-09  Michele Tavella <michele.tavella@epfl.ch>
+%
+% This file is a recursive include script and is meant to be used with
+% mtpath:
+% - mtpath is distributed with the cnbitoolkit: if you are using the tobicore
+% MEX intefaces via cnbitoolkit, mtpath will be used to load tobicoremat
+% automatically
+% - mtpath can be downloaded from:
+%  http://www.mtvl.org/projects/mtpath/
+% - still, tobicoremat does not need mtpath to work
+
 [~, basepath] = mtpath_basename(which('tobicoremat_init.m'));
 
 mtpath_include([basepath '/mextobicore']);
