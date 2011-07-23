69f8c93cd5e17919e0e855ff42a281feca3a418d
Michele Tavella <tavella.michele@gmail.com>
2011-07-23 19:00:08 +0200

    Initial layout for TPSocket and TPHost

--- src/tobiplatform/TPSocket.cpp
+++ src/tobiplatform/TPSocket.cpp
@@ -1,12 +1,54 @@
+/*
+    Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+    Michele Tavella <michele.tavella@epfl.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef TPSOCKET_CPP 
 #define TPSOCKET_CPP 
 
 #include "TPSocket.hpp" 
+#include <stdlib.h>
+#include <string.h>
 
-TPSocket::TPSocket(void) {
+TPHost::TPHost(void) {
+	memset(this->address, 0, TPHost::AddressSize);
+	this->port = 0;
+}
+
+TPSocket::TPSocket(size_t bsize) {
+	this->_bsize = bsize;
+	this->_mc = 256;
 }
 
 TPSocket::~TPSocket(void) {
 }
+
+void TPSocket::Init(void) {
+	this->_fd = 0;
+	this->_mc = 0;
+	
+	this->_buffer = malloc(this->_bsize);
+	memset(this->_buffer, 0, this->_bsize);
+	
+	memset(&(this->_address), 0, sizeof(this->_address));
+	memset(&(this->_results), 0, sizeof(this->_results));
+	memset(&(this->_endpoint), 0, sizeof(this->_endpoint));
+
+	this->_bsizemax = 0;
+}
+
 
 #endif
--- src/tobiplatform/TPSocket.hpp
+++ src/tobiplatform/TPSocket.hpp
@@ -1,16 +1,73 @@
+/*
+    Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+    Michele Tavella <michele.tavella@epfl.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef TPSOCKET_HPP 
 #define TPSOCKET_HPP 
 
-class TPSocket {
+#include <arpa/inet.h>
+#include <netdb.h>
+
+/*! \brief libtransport host 
+ * This structure holds whatever identifies an host
+ */
+class TPHost {
 	public:
-		TPSocket(void);
-		virtual ~TPSocket(void);
-	private:
-	protected:
+		TPHost(void);
 
 	public:
-	private:
+		const static size_t AddressSize = INET_ADDRSTRLEN + 1;
+		//! Address (e.g.: 127.0.0.1)
+		char address[TPHost::AddressSize];
+		//! Port (e.g.: 8000)
+		short unsigned int port;
+};
+
+
+/*! \brief TCP/UDP socket
+ */
+class TPSocket {
+	public:
+		TPSocket(size_t bsize);
+		virtual ~TPSocket(void);
 	protected:
+		void Init(void);
+
+	public:
+		//! Host informations associated to the current socket
+		TPHost local;
+		TPHost remote;
+	protected:
+		//! File descriptor
+		int _fd;
+		//! Socket address
+		struct sockaddr_in _address;
+		struct sockaddr_storage _endpoint;
+		//! Buffer 
+		void* _buffer;
+		size_t _bsize;
+		//! Maximum number of connections
+		unsigned int _mc;
+		//! Socket info
+		struct addrinfo _results;
+		//! Socket info
+		struct addrinfo* _info;
+		//! Maximum size of message
+		size_t _bsizemax;
 };
 
 #endif
