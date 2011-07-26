1ca36a65cfc4d8b4214a8547b0c2273a1f58e75a
unknown <mtavella@.(none)>
2011-07-26 20:19:19 +0200

    Now compiling MEX

--- mex/mextobiic/make.m
+++ mex/mextobiic/make.m
@@ -1,7 +1,7 @@
 % 2011-06-04  Michele Tavella <michele.tavella@epfl.ch>
 disp('Building MEX interface');
 try 
-	mex tobiic.cc c:\minGW\lib\libtobiic.a c:\minGW\lib\libtobicore.a
+	mex tobiic.cc c:\minGW\lib\libtobiic.dll.a c:\minGW\lib\libtobicore.dll.a
 	exit;
 catch e
 	disp('Error: cannot compile');
