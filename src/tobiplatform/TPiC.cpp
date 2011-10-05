20c9cbf3e9c36630aac6943adbf2a9daed6e17df
Michele Tavella <tavella.michele@gmail.com>
2011-10-05 12:47:08 +0200

    Initial TPiC layout

--- src/tobiplatform/TPiC.cpp
+++ src/tobiplatform/TPiC.cpp
@@ -1,12 +1,87 @@
+/*
+    Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+    Michele Tavella <michele.tavella@epfl.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef TPIC_CPP 
 #define TPIC_CPP 
 
 #include "TPiC.hpp" 
 
 TPiC::TPiC(void) {
+	this->_socket = NULL;
+	this->_endpoint = NULL;
+	this->_com = NULL;
 }
 
 TPiC::~TPiC(void) {
+}
+
+/*
+bool TPiC::Send(const std::string& message) {
+	return false;
+}
+
+bool TPiC::Recv(std::string* message) {
+	return false;
+}
+*/
+
+bool TPiC::Configure(const std::string &ip, const std::string& port, int mode) {
+	switch(mode) {
+		case TPiC::AsServer:
+			return this->ConfAsServer(ip, port);
+			break;
+		case TPiC::AsClient:
+			return this->ConfAsClient(ip, port);
+			break;
+	}
+	return false;
+}
+
+
+bool TPiC::ConfAsServer(const std::string &ip, const std::string& port) {
+	if(this->_socket != NULL)
+		return false;
+
+	bool status = true;
+	this->_socket = new TPSocket(TPSocket::TCP);
+	status &= this->_socket->Open(true);
+	status &= this->_socket->Bind(ip, port);
+	status &= this->_socket->Listen();
+	
+	this->_endpoint = new TPSocket(TPSocket::TCP);
+	status &= this->_socket->Accept(this->_endpoint);
+	
+	this->_com = this->_endpoint;
+
+	return status;
+}
+
+bool TPiC::ConfAsClient(const std::string &ip, const std::string& port) {
+	if(this->_socket != NULL)
+		return false;
+	
+	this->_socket = new TPSocket(TPSocket::TCP);
+	this->_socket->Open(false);
+	this->_socket->Connect(ip, port);
+	
+	this->_com = this->_socket;
+
+	return false;
 }
 
 #endif
--- src/tobiplatform/TPiC.hpp
+++ src/tobiplatform/TPiC.hpp
@@ -1,16 +1,47 @@
+/*
+    Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+    Michele Tavella <michele.tavella@epfl.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef TPIC_HPP 
 #define TPIC_HPP 
 
+#include "TPSocket.hpp"
+
+/*! \brief Simple iC interface
+ */
 class TPiC {
 	public:
 		TPiC(void);
 		virtual ~TPiC(void);
+		bool Send(void);
+		bool Recv(void);
+		bool Configure(const std::string &ip, const std::string& port,
+				int mode = TPiC::AsServer);
 	private:
-	protected:
+		bool ConfAsServer(const std::string &ip, const std::string& port);
+		bool ConfAsClient(const std::string &ip, const std::string& port);
 
 	public:
+		const static int AsServer = 0;
+		const static int AsClient = 1;
 	private:
-	protected:
+		TPSocket* _socket;
+		TPSocket* _endpoint;
+		TPSocket* _com;
 };
 
 #endif
