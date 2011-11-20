1c6af19921c8016bb21beeb024848d7658a703c4
Michele Tavella <tavella.michele@gmail.com>
2011-11-20 13:34:19 +0100

    doc: Working on initial page

--- src/tobiic/ICTypes.hpp
+++ src/tobiic/ICTypes.hpp
@@ -101,12 +101,53 @@ typedef std::string ICLabel;
 /*! \mainpage tobicore
  *
  * \section sec_intro Introduction
- * todo
+ * The tobicore project provides a total of four C++ libraries that implement
+ * some concepts of the <a href="http://www.tobi-project.org">TOBI</a> hBCI design.
+ *
+ * \subsection sec_intro1 libtobicore 
+ * Low-level library that provides basic functionalities, such as timestamp
+ * management (via TCTimestamp) and the TCBlock base class. 
+ *
+ * \subsection sec_intro2 libtobiic 
+ * Implements the TOBI iC classifier output encoding/decoding via ICMessage.
+ *
+ * \subsection sec_intro3 libtobiid 
+ * Implements the TOBI iD events encoding/decoding via IDMessage.
+ *
+ * \subsection sec_intro4 libtobiplatform
+ * Provides working and cross-platform implementations of some TOBI components,
+ * such as an iC client/server class (TPiC) and an iD client class (TPiD).
+ * 
+ * \subsection sec_intro5 tobicoremat: mextobicore, mextobiic and mextobiid
+ * tobicoremat provides MEX interfaces to a large set of the tobicore
+ * functionalities.
+ *
+ * \section sec_resources Resources
+ * - <a href="http://www.tobi-project.org">TOBI project website</a>: general
+ * resources about the big BCI project responsible about the hBCI design 
+ * - <a href="http://www..org">BCI standards website</a>: lots of information
+ * about hBCI and BCI standards in general
+ * - <a href="http://files.mtvl.org/projects/tobicore/doc/libtobiic.pdf">TOBI iC
+ * for dummies</a>: this guide was written to make sure non-technical people
+ * could understand the basis of TOBI iC. Do not expect design documents and
+ * other boring stuff. You will mainly find user-cases and examples there.
+ * Still, it is a must-read document before starting.
+ * - <a href="http://files.mtvl.org/projects/tobicore/doc/libtobiid.pdf">TOBI iD
+ * for dummies</a>: as before.
  *
  * \section sec_install Install
+ * The tobicore libraries and tools are available on multiple platforms, and the
+ * author provides ready-to-use packages. The rule of thumb is:
+ * - Ubuntu and Debian Linux 32bit/64bit are always supported via .deb packages
+ * - Windows support is limited to 32 bit and, as today, only for Windows 7
+ * - No package is provided for other platforms
+ * - GNU automake and autoconf make it easy to build the libraries on other
+ * platforms
+ *
+ * \subsection sec_install_linux_source Tarball
  * todo
  *
- * \subsection sec_install_linux_source Tarball/Git
+ * \subsection sec_install_linux_git Git
  * todo
  *
  * \subsection sec_install_linux_deb Debian packages
@@ -114,13 +155,28 @@ typedef std::string ICLabel;
  * 
  * \subsection sec_install_windows Microsoft Windows
  * todo
- *
- * \section sec_resources Resources
- * todo
+ * 
+ * \section sev_version Versioning
+ * tobicore versions follow the $CURRENT.$REVISION.$AGE schema and the following
+ * rules:
+ * - If the library source code has changed at all since the last update,
+ * then increment $REVISION
+ * - If any interfaces have been added, removed, or changed since the last
+ * update, increment $CURRENT, and set $REVISION to 0
+ * - If any interfaces have been added since the last public release, then
+ * increment $AGE
+ * - If any interfaces have been removed since the last public release, then
+ * set $AGE to 0
  *
  * \section sec_author Author
- * todo
+ * tobicore is written, documented and maintained by Michele Tavella
+ * <michele.tavella@epfl.ch>.
  *
+ * \section sec_ack Acknoledgment
+ * This work is supported by thegno European ICT Programme Project FP7-224631,
+ * TOBI: Tools for Brain-Computer Interaction. This paper only reflects the
+ * authors' views and funding agencies are not liable for any use that may be made
+ * of the information contained herein.
  */
 
 /*! \example icexample.cpp
@@ -162,7 +218,7 @@ typedef std::string ICLabel;
  * TOBI plaform iD client program.
  */
 
-/*! \example tpservertcp.cpp
+/*! \example tptcpserver.cpp
  * TOBI plaform TCP server example.
  */
 
@@ -174,14 +230,6 @@ typedef std::string ICLabel;
  * TOBI plaform iC server program.
  */
 
-/*! \example tpclienttcp.cpp
+/*! \example tptcpclient.cpp
  * TOBI plaform TCP client example.
- */
-
-/*! \example tpic.cpp
- * TOBI plaform iC program.
- */
-
-/*! \example tpid.cpp
- * TOBI plaform iD program.
  */
